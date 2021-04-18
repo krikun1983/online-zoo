cf68d887a1f99c1aa77db528e01a18cf73ac2dd9
Stanislav Akhmadeev <akhmadeevst@gmail.com>
2021-04-18 23:59:00 +0400

    feat: add burger

--- online-zoo/assets/js/script.js
+++ online-zoo/assets/js/script.js
@@ -1,4 +1,4 @@
-//Ограничеваю фрму с донатом в 4 символа
+//Ограничеваю форму с донатом в 4 символа
 document.querySelector('.donation__input').onclick = numberLimitation;
 
 function numberLimitation(input, limit) {
@@ -22,6 +22,14 @@ function numberLimitation(input, limit) {
 }
 
 numberLimitation("#donations", 4);
+
+//Выдвижение меню в header на 640
+document.querySelector('.burger-header').onclick = function () {
+  let header = document.querySelector(".nav-header");
+  let burgerHeader = document.querySelector(".burger-header");
+  burgerHeader.classList.toggle("burger-header-active");
+  header.classList.toggle("nav-header-show");
+}
 
 //меняю стрелку у кнопок
 
